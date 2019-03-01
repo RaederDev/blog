0b9fa155128d79bf795522a60f167f619b2c5682
N0ps32 <benjamin@raeder.technology>
2019-03-01 01:13:10 +0100

    adapted dev scripts and updated craftcms

--- bin/start_dev.sh
+++ bin/start_dev.sh
@@ -13,6 +13,14 @@ grep -v '10.10.*'
 )
 
 docker run --rm \
+    --name blog-db \
+    -v $(pwd)/.mysql:/var/lib/mysql \
+    -e MYSQL_ROOT_PASSWORD=password \
+    -d \
+    -p 3306:3306 \
+    mariadb:10.3
+
+docker run --rm \
     --name blog \
     -v "$(pwd)/blog":/srv \
     -v "$(pwd)/docker-assets/Caddyfile":/etc/Caddyfile \
@@ -20,3 +28,5 @@ docker run --rm \
     -p 8081:2016 \
     --add-host "parent:$ip" \
     blog
+
+docker stop blog-db
